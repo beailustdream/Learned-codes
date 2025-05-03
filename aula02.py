1563a6493712c3f71f5ab6410feec47da96ab0c6
Beatriz G.S <beagoncalvessilva0@gmail.com>
2025-05-03 12:21:09 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,8 @@
+codigo = int(input("Digite o código do produto: "))
+nome = input("Digite o nome do produto: ")
+salario = float(input("Digite o salário do funcionário: "))
+ativo = True
+print("Código do produto: %d "%codigo)
+print("Nome do produto: %s "% nome)    
+print("Salário do funcionário: %.2f "%salario)
+print("Ativo: %r" %ativo)
