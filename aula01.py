c96d450cac05f4b0e3e91872c72d60e170f63ab8
Beatriz G.S <beagoncalvessilva0@gmail.com>
2025-05-03 12:02:27 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,12 @@
+# Aula 01 - Python
+# Exemplo de código Python
+print("Hello, World!") 
+# Exemplo imput() e print() combinados com variáveis.
+fruta = input("Digite o nome de uma fruta: ")
+print("A fruta digitada foi: ", fruta)
+# Entrada de dados não string.
+codigo = int(input("Digite o código do produto: "))
+nome = input("Digite o nome do produto: ")
+salario = float(input("Digite o salário do funcionário: "))
+ativo = True
+
